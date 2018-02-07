2b98b325a545d436c14cd75dcee56dda919e9dec
shisp <shisp@2345.com>
2018-02-07 20:14:51 +0800

    4.eureka-consumer-rbbon

---
+++ source/eureka-consumer-ribbon/src/main/java/xyz/Controller/DcController.java
@@ -0,0 +1,27 @@
+package xyz.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class DcController {
+
+//    @Autowired
+//    private LoadBalancerClient loadBalancerClient;
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/consumer/ribbon/dc")
+    public String dc() {
+//        ServiceInstance serviceInstance = loadBalancerClient.choose("eureka-client");
+//        String url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/dc";
+        
+        //eureka-client,ribbon内置拦截器会替换eureka-client服务对应host
+        String url = "http://eureka-client/dc";
+        String result = restTemplate.getForObject(url, String.class);
+        System.out.println("get message from client dc" + result);
+        return result;
+    }
+}
